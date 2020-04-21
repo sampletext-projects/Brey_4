8090464e69722d988e6ed555987bcfbb96c1c926
sampletext32 <sampletext32@bk.ru>
2020-04-21 20:24:45 +0300

    Add project

---
+++ Brey_4/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	cout << "Программу сделала Белевцева Дарья Юрьевна студент группы УМЛ-112\n";
+	cout << "Программа для расчёта банковского вклада\n";
+
+	double k = 1.12;
+	double income = 5;
+
+	double amount = 0;
+	for (int i = 0; i <= 18; i++)//т.к. на 18 лет тоже даёт, то <=
+	{
+		amount *= k;
+		amount += income;
+		cout << i << " лет прошло, на вкладе " << amount << endl;
+	}
+	
+
+	system("pause");
+
+	return 0;
+}
